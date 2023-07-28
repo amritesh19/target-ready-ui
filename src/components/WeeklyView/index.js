83dfc40ab11ee3860d661b62b5f897f547983f68
Amritesh Anand <amriteshanand19@gmail.com>
2023-07-28 14:04:15 +0530

    Added Modal Component & simplified css for week view

--- src/components/WeeklyView/index.js
+++ src/components/WeeklyView/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { styled } from "@mui/material/styles";
 import {
   TableContainer,
@@ -7,22 +7,39 @@ import {
   TableRow,
   TableCell,
   TableBody,
-  Paper,
   Card,
   CardContent,
-  Grid,
-  Box
+  Modal,
+  Typography,
 } from "@mui/material";
-import './index.css';
-import { left } from "@popperjs/core";
+import "./index.css";
 
 const CustomTableContainer = styled(TableContainer)({
   maxWidth: 900,
   margin: "auto",
   marginTop: "16px",
+  width: 1900,
+  height: 450,
+  marginLeft: "-20%",
+  overflow: "auto",
 });
 
-const TimeTable = () => {
+const WeeklyView = () => {
+  const [selectedCellData, setSelectedCellData] = useState(null);
+  const [openModal, setOpenModal] = useState(false);
+
+  const handleCellClick = (day, time) => {
+    const cellData = scheduleData.find(
+      (item) => item.day === day && item.time === time
+    );
+    setSelectedCellData(cellData);
+    setOpenModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
+
   const days = [
     "Monday",
     "Tuesday",
@@ -50,73 +67,56 @@ const TimeTable = () => {
 
   return (
     <>
-    {/* <Grid
-      container
-      justifyContent="center"
-      alignItems="center"
-      //style={{ height: "70vh" }}
-    > */}
-    <Card
-    sx={{
-      backgroundColor:'#aabce7',
-      marginLeft:'45%',
-      marginRight:'45%',
-      marginTop:'1%',
-      fontWeight:'bold'
-    }}
-    >
-      {/* <CardContent>View: Weekly</CardContent> */}
-    </Card>
-    {/* </></Grid>  */}
-    {/* 
-    <Box
-    sx={{
-      marginLeft:'50%',
-      marginTop:'10%',
-      width: 100,
-      height: 50,
-      backgroundColor: '#aabce7;'
-    }}
-    >Hello</Box> */}
-    <CustomTableContainer component={Paper}
-    sx={{
-      width: 1900,
-      height: 450,
-      marginLeft:'-20%',
-      overflow: 'auto'
-      
-    }}
-    >
-      <Table >
-        <TableHead>
-          <TableRow >
-            <TableCell id="bold">Time Slot</TableCell>
-            {timeSlots.map((slot) => (
-              <TableCell key={slot}>{slot}</TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {days.map((day) => (
-            <TableRow key={day}>
-              <TableCell id="bold">{day}</TableCell>
-              {timeSlots.map((slot) => {
-                const cellData = scheduleData.find(
-                  (item) => item.day === day && item.time === slot
-                );
-                return (
-                  <TableCell key={`${slot}-${day}`}>
-                    {cellData ? cellData.subject : ""}
-                  </TableCell>
-                );
-              })}
+      <CustomTableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell id="bold">Time Slot</TableCell>
+              {timeSlots.map((slot) => (
+                <TableCell key={slot}>{slot}</TableCell>
+              ))}
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </CustomTableContainer>
+          </TableHead>
+          <TableBody>
+            {days.map((day) => (
+              <TableRow key={day}>
+                <TableCell id="bold">{day}</TableCell>
+                {timeSlots.map((slot) => {
+                  const cellData = scheduleData.find(
+                    (item) => item.day === day && item.time === slot
+                  );
+                  return (
+                    <TableCell
+                      key={`${slot}-${day}`}
+                      onClick={() => handleCellClick(day, slot)}
+                    >
+                      {cellData ? cellData.subject : ""}
+                    </TableCell>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </CustomTableContainer>
+
+      <Modal open={openModal} onClose={handleCloseModal}>
+        <Card className="modal-card">
+          <CardContent>
+            <Typography variant="h6">
+              Subject: {selectedCellData?.subject}
+            </Typography>
+            <Typography variant="body1">
+              Day: {selectedCellData?.day}
+            </Typography>
+            <Typography variant="body1">
+              Time: {selectedCellData?.time}
+            </Typography>
+          </CardContent>
+        </Card>
+      </Modal>
     </>
   );
 };
 
-export default TimeTable;
+export default WeeklyView;
