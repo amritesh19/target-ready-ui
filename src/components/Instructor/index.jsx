e88fa894d78943b0123af81c2d373b7f3222cb59
Arnick-s <iamarvind.hi@gmail.com>
2023-07-30 15:30:29 +0530

    Instructor table added

--- src/components/Instructor/index.jsx
+++ src/components/Instructor/index.jsx
@@ -1,9 +1,41 @@
 import { Card } from "@mui/material";
+import React from 'react';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+
 
 const Instructor = () => {
+
+  const instructorData = [
+    { serialNumber: 1, instructorId: 'ID001', instructorName: 'John Doe', instructorContact: 'john@example.com' },
+    { serialNumber: 2, instructorId: 'ID002', instructorName: 'Jane Smith', instructorContact: 'jane@example.com' },
+    // Add more instructor data here
+  ];
+
   return (
     <Card className="App-Card">
       <h3>List of Instructors</h3>
+      <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Serial Number</TableCell>
+            <TableCell>Instructor ID</TableCell>
+            <TableCell>Instructor Name</TableCell>
+            <TableCell>Instructor Contact</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {instructorData.map((instructor) => (
+            <TableRow key={instructor.instructorId}>
+              <TableCell>{instructor.serialNumber}</TableCell>
+              <TableCell>{instructor.instructorId}</TableCell>
+              <TableCell>{instructor.instructorName}</TableCell>
+              <TableCell>{instructor.instructorContact}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
     </Card>
   );
 };
