27a39b16285a81aa0c4870393c9d98433525be98
Amritesh Anand <amriteshanand19@gmail.com>
2023-07-31 13:51:15 +0530

    Added axios support for instructor component

--- src/components/Instructor/index.jsx
+++ src/components/Instructor/index.jsx
@@ -1,55 +1,110 @@
 import { Card } from "@mui/material";
-import React from 'react';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
-
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@mui/material";
 
 const Instructor = () => {
+  const [instructorData, setInstructorData] = useState([]);
 
-  const instructorData = [
-    { serialNumber: 1, instructorId: 'ID001', instructorName: 'John Doe', instructorContact: 'john@example.com' },
-    { serialNumber: 2, instructorId: 'ID002', instructorName: 'Jane Smith', instructorContact: 'jane@example.com' },
-    // Add more instructor data here
-  ];
+  useEffect(() => {
+    axios
+      .get("http://localhost:8085/admin/instructors")
+      .then((response) => {
+        setInstructorData(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching instructor data:", error);
+      });
+  }, []);
 
   const myStyle = {
     display: "flex",
     alignItem: "center",
     height: "100%",
-    border:'5px solid black',
-    background:'blue'
-
-  }
+    border: "5px solid black",
+    background: "blue",
+  };
   const myStyle2 = {
-    position:"absolute",
-    top:"50%",
-    left:"30%",
+    position: "absolute",
+    top: "50%",
+    left: "30%",
     transform: "translate(0px,-50%)",
-  }
+  };
   return (
     <Card className="App-Card" style={myStyle2}>
-      <h3 style={{border:'1px solid black',padding:'15px'}}>List of Instructors</h3>
-      <TableContainer component={Paper} >
-      <Table>
-        <TableHead>
-          <TableRow style={{border:'1px solid black'}}>
-            <TableCell style={{fontWeight:'bold', background:'#aabce7',border:'1px solid black' }}>Serial Number</TableCell>
-            <TableCell style={{fontWeight:'bold', background:'#aabce7',border:'1px solid black'}}>Instructor ID</TableCell>
-            <TableCell style={{fontWeight:'bold', background:'#aabce7',border:'1px solid black'}}>Instructor Name</TableCell>
-            <TableCell style={{fontWeight:'bold', background:'#aabce7',border:'1px solid black'}}>Instructor Contact</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {instructorData.map((instructor) => (
-            <TableRow key={instructor.instructorId} >
-              <TableCell style={{border:'1px solid black'}}>{instructor.serialNumber}</TableCell>
-              <TableCell style={{border:'1px solid black'}}>{instructor.instructorId}</TableCell>
-              <TableCell style={{border:'1px solid black'}}>{instructor.instructorName}</TableCell>
-              <TableCell style={{border:'1px solid black'}}>{instructor.instructorContact}</TableCell>
+      <h3 style={{ border: "1px solid black", padding: "15px" }}>
+        List of Instructors
+      </h3>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow style={{ border: "1px solid black" }}>
+              <TableCell
+                style={{
+                  fontWeight: "bold",
+                  background: "#aabce7",
+                  border: "1px solid black",
+                }}
+              >
+                Serial Number
+              </TableCell>
+              <TableCell
+                style={{
+                  fontWeight: "bold",
+                  background: "#aabce7",
+                  border: "1px solid black",
+                }}
+              >
+                Instructor ID
+              </TableCell>
+              <TableCell
+                style={{
+                  fontWeight: "bold",
+                  background: "#aabce7",
+                  border: "1px solid black",
+                }}
+              >
+                Instructor Name
+              </TableCell>
+              <TableCell
+                style={{
+                  fontWeight: "bold",
+                  background: "#aabce7",
+                  border: "1px solid black",
+                }}
+              >
+                Instructor Contact
+              </TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {instructorData.map((instructor, index) => (
+              <TableRow key={instructor.instructor_id}>
+                <TableCell style={{ border: "1px solid black" }}>
+                  {index + 1}
+                </TableCell>
+                <TableCell style={{ border: "1px solid black" }}>
+                  {instructor.instructor_id}
+                </TableCell>
+                <TableCell style={{ border: "1px solid black" }}>
+                  {instructor.instructor_name}
+                </TableCell>
+                <TableCell style={{ border: "1px solid black" }}>
+                  {instructor.instructor_contact}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Card>
   );
 };
